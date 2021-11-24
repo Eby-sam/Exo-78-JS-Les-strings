13a9f9daca1d64e6f7dd21be29ec70a1cfcc222f
coque <coquelet.samuel@gmail.com>
2021-11-24 16:22:46 +0100

    fini

--- exercice1.js
+++ exercice1.js
@@ -1,14 +1,28 @@
-let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
-
+let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
+    "incididunt ut labore et dolore magna aliqua";
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
-
+alert(monTexte.length);
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let position = monTexte.indexOf("i");
+console.log(position);
 
+let positionID = document.getElementById("position");
+positionID.innerHTML = position;
 
+let newText = monTexte.toUpperCase(monTexte);
+console.log(newText);
+
+let maChaine = document.getElementById("chaineMaj");
+maChaine.innerHTML = newText;
+
+for(let i = 0;i < a.length; i++){
+    if(i === maChaine.length){
+        alert("lettre a trouvé");
+    }
+}
 
 
 
